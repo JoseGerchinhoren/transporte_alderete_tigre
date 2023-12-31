20896b0635ccf0954e081ca41bf2ce5fc4915926
JoseGerchinhoren <josegerchinhoren97@gmail.com>
2023-12-30 21:15:00 -0300

    .

--- config.py
+++ config.py
@@ -4,16 +4,16 @@ import streamlit as st
 
 def cargar_configuracion():
 
-    #Configuracion Local
-    # Cargar configuración desde el archivo config.json
-    with open("../config.json") as config_file:
-        config = json.load(config_file)
+    # #Configuracion Local
+    # # Cargar configuración desde el archivo config.json
+    # with open("../config.json") as config_file:
+    #     config = json.load(config_file)
 
-    # Desempaquetar las credenciales desde el archivo de configuración
-    aws_access_key = config["aws_access_key"]
-    aws_secret_key = config["aws_secret_key"]
-    region_name = config["region_name"]
-    bucket_name = config["bucket_name"]
+    # # Desempaquetar las credenciales desde el archivo de configuración
+    # aws_access_key = config["aws_access_key"]
+    # aws_secret_key = config["aws_secret_key"]
+    # region_name = config["region_name"]
+    # bucket_name = config["bucket_name"]
 
     # # Configuracion Render
     # # Configura tus credenciales y la región de AWS desde variables de entorno
@@ -22,10 +22,10 @@ def cargar_configuracion():
     # region_name = os.getenv('aws_region')
     # bucket_name = 'megatron-accesorios'
 
-    # #Configuracion Streamlit
-    # aws_access_key = st.secrets["aws_access_key"]
-    # aws_secret_key = st.secrets["aws_secret_key"]
-    # region_name = st.secrets["region_name"]
-    # bucket_name = st.secrets["bucket_name"]
+    #Configuracion Streamlit
+    aws_access_key = st.secrets["aws_access_key"]
+    aws_secret_key = st.secrets["aws_secret_key"]
+    region_name = st.secrets["region_name"]
+    bucket_name = st.secrets["bucket_name"]
 
     return aws_access_key, aws_secret_key, region_name, bucket_name
